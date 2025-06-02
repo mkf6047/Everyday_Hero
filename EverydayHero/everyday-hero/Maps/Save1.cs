c13f6d07b28134978d97cde5a3f445fbb598784b
CAZtheArcher <caz1158@g.rit.edu>
2025-06-02 10:41:27 -0400

    replaced the carpet, and added save slots

---
+++ EverydayHero/everyday-hero/Maps/Save1.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Save1 : Button
+{
+    PlayerVariables PlayerInfo;
+    public override void _Ready()
+    {
+        base._Ready();
+        this.Pressed += Clicked;
+        PlayerInfo = GD.Load<PackedScene>("res://PlayerInformation/PlayerVariables.tscn").Instantiate<PlayerVariables>();
+        using var file = FileAccess.Open("user://save_game_1.dat", FileAccess.ModeFlags.Read);
+        string content = file.GetAsText();
+        PlayerInfo.coin = Int32.Parse(content);
+    }
+
+    private void Clicked()
+    {
+        GetTree().Root.AddChild(PlayerInfo);
+        GetTree().ChangeSceneToFile("res://Maps/MainScene.tscn");
+    }
+
+}
---
+++ EverydayHero/everyday-hero/PlayerInformation/PlayerVariables.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class PlayerVariables : Node2D
+{
+    public int coin = 0;
+}
