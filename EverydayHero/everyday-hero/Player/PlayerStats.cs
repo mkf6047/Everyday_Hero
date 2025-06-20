b01fd55f1eb65fea98dce2b1818cd3b4903f90b6
CAZtheArcher <caz1158@g.rit.edu>
2025-06-20 11:44:48 -0400

    player stats has room for money

--- EverydayHero/everyday-hero/Player/PlayerStats.cs
+++ EverydayHero/everyday-hero/Player/PlayerStats.cs
@@ -3,4 +3,10 @@ using System;
 
 public partial class PlayerStats : Node
 {
+    private int coins = 0;
+    public int Coins
+    {
+        get { return coins; }
+        set { coins = value; }
+    }
 }
