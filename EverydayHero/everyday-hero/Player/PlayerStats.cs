858010678eab176656573de2f22a14d838e7ccea
CAZtheArcher <caz1158@g.rit.edu>
2025-06-20 12:36:33 -0400

    updated playerstats to be accessed from anywhere

--- EverydayHero/everyday-hero/Player/PlayerStats.cs
+++ EverydayHero/everyday-hero/Player/PlayerStats.cs
@@ -3,10 +3,17 @@ using System;
 
 public partial class PlayerStats : Node
 {
+    public static PlayerStats Instance { get; private set; }
     private int coins = 0;
     public int Coins
     {
         get { return coins; }
         set { coins = value; }
     }
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
 }
