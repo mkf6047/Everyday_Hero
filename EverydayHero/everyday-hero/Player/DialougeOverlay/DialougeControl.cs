15a0b118c831a91f3c4ba21a13e9b46f544cd801
CAZtheArcher <caz1158@g.rit.edu>
2025-07-08 15:30:08 -0400

    updated blacksmith sprite & moved NPC sprites to NPC folder

--- EverydayHero/everyday-hero/Player/DialougeOverlay/DialougeControl.cs
+++ EverydayHero/everyday-hero/Player/DialougeOverlay/DialougeControl.cs
@@ -4,12 +4,18 @@ using System;
 public partial class DialougeControl : Node2D
 {
     RichTextLabel textbox;
+    RichTextLabel nameplate;
     public override void _Ready()
     {
         textbox = (RichTextLabel)GetNode("./Textbox");
+        nameplate = (RichTextLabel)GetNode("./Nameplate");
     }
     public void UpdateTextbox(string newText)
     {
         textbox.Text = "" + newText + "";
+    }
+    public void UpdateNameplate(string newText)
+    {
+        nameplate.Text = "" + newText + "";
     }
 }
