4364def47d228f57f1b1cbc96edaec9acedc8d44
CAZtheArcher <caz1158@g.rit.edu>
2025-07-23 09:58:08 -0400

    template for static NPC built

---
+++ EverydayHero/everyday-hero/NPC/StaticNPC/NPCCollide.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public partial class NPCCollide : Area2D
+{
+    bool isColliding;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        isColliding = false;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if (isColliding) {
+            if (Input.IsActionPressed("Interact"))
+            {
+                
+            }
+        }
+    }
+
+
+
+    public virtual void OnArea2DBodyEntered(Node2D body)
+    {
+        try
+        {
+            Player player = (Player)body;
+            isColliding = true;
+            Sprite2D notice = (Sprite2D)GetNode("../../Player/PlayerBody2D/Notice");
+            notice.Call("ShowNotice");
+        }
+        catch
+        {
+            GD.PrintErr("Interactive item just collided with something other than a Player.");
+        }
+    }
+
+    public virtual void OnArea2DBodyExited(Node2D body)
+    {
+        try
+        {
+            Player player = (Player)body;
+            isColliding = false;
+            Sprite2D notice = (Sprite2D)GetNode("../../Player/PlayerBody2D/Notice");
+            notice.Call("HideNotice");
+        }
+        catch
+        {
+            GD.PrintErr("Interactive item just stopped colliding with something other than a Player.");
+        }
+    }
+}
---
+++ EverydayHero/everyday-hero/NPC/StaticNPC/StaticNpc.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class StaticNpc : Node2D
+{
+    [Export]
+    public Texture2D sprite;
+    [Export]
+    public string dialougeFilepath;
+
+    Sprite2D npcSprite;
+
+    public override void _Ready()
+    {
+        npcSprite = (Sprite2D)GetNode("./NPCRigidBody2D/NPCSprite");
+        npcSprite.Texture = sprite;
+    }
+
+}
