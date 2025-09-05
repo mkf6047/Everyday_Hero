1a27fde8ab533047367e652f8370659c494e72ea
CAZtheArcher <caz1158@g.rit.edu>
2025-09-05 12:18:57 -0400

    added collision area to trigger tutorials

---
+++ EverydayHero/everyday-hero/Player/Tutorial Overlay/Tutorial Trigger/TriggerArea.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class TriggerArea : Area2D
+{
+
+
+    public virtual void OnArea2DBodyEntered()
+    {
+        
+    }
+
+    public virtual void OnArea2DBodyExited()
+    {
+        
+    }
+}
