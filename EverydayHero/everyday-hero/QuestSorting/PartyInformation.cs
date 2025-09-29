8333639e62d21c968d3b689168aefc93f8d102af
CAZtheArcher <caz1158@g.rit.edu>
2025-09-29 13:51:14 -0400

    QSS set up with required sprites & areas

---
+++ EverydayHero/everyday-hero/QuestSorting/PartyInformation.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class PartyInformation : Node2D
+{
+    RichTextLabel leaderSpeech;
+
+
+    public override void _Ready()
+    {
+        
+    }
+
+}
